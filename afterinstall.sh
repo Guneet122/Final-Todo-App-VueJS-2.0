db295887cf3eace492edb53e8ac659b1bab06909
Guneet122 <102280641+Guneet122@users.noreply.github.com>
2022-04-20 16:02:08 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -8,10 +8,8 @@ npm install
 npm install pm2 -g
 sudo amazon-linux-extras install nginx1
 sudo systemctl start nginx
-cd
 cd /etc/nginx/
 sed -i '38,55d' nginx.conf
 cd conf.d/
 sudo touch todo.conf
 echo -e "server {\n listen 80 default_server;\n server_name _;\n\n # node api reverse proxy\n location / {\n  proxy_pass http://localhost:3000/;\n }\n}" >> todo.conf;
-cd
